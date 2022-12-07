7ef42a5dfe2301004119c15ebeb8b3e999eb734f
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2022-12-07 21:48:24 +0200

    Fix array init (commit actually by B. Voloshkevych but he couldn't send it so I sent it)

--- src/com/teamproject/encryptor/mvc/AESCipher.java
+++ src/com/teamproject/encryptor/mvc/AESCipher.java
@@ -1,6 +1,5 @@
 package com.teamproject.encryptor.mvc;
 
-import java.util.Arrays;
 import java.security.InvalidKeyException;
 import java.security.SecureRandom;
 
@@ -20,12 +19,12 @@ import java.security.InvalidAlgorithmParameterException;
 import java.util.Base64;
 
 abstract class SymmetricCipher {
-	public SecretKeySpec keySpec;
-	public IvParameterSpec iv;
-	public Cipher cipher;
-	
-	public SymmetricCipher(String password) {
-		SecureRandom random = new SecureRandom();
+  public SecretKeySpec keySpec;
+  public IvParameterSpec iv;
+  public Cipher cipher;
+  
+  public SymmetricCipher(String password) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+    SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
         PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256); // AES-256
@@ -35,53 +34,53 @@ abstract class SymmetricCipher {
         byte[] ivBytes = new byte[16];
         random.nextBytes(ivBytes);
         IvParameterSpec iv = new IvParameterSpec(ivBytes);
-		
-		this.iv = iv;
-		this.keySpec = keySpec;
-		this.cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	}
-	
-	public abstract String encrypt(String text);
-	public abstract String decrypt(String text);
+    
+    this.iv = iv;
+    this.keySpec = keySpec;
+    this.cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+  }
+  
+  public abstract String encrypt(String text);
+  public abstract String decrypt(String text);
 }
 
 
 public class AESCipher extends SymmetricCipher {
-	byte[] _inputBytes;
-	byte[] _outputBytes;
+  byte[] _inputBytes;
+  byte[] _outputBytes;
 
-	public AESCipher(String password) {
-		super(password);
-	}
+  public AESCipher(String password) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
+    super(password);
+  }
 
-	public String encrypt(String text) {
-		byte[] inputBytes = text.getBytes();
-		byte[] outputBytes;
-		
-		try {
-			cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
-			outputBytes = cipher.doFinal(inputBytes);
-			
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidKeySpecException | InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		}
-		
-		_inputBytes = inputBytes;
-		_outputBytes = outputBytes;
-		return Base64.getEncoder().encodeToString(outputBytes);
-	}
-	
-	public String decrypt(String text) {
-		_inputBytes = Base64.getDecoder().decode(text);
-		
-		try {
-			cipher.init(Cipher.DECRYPT_MODE, keySpec, iv);
-			_outputBytes = cipher.doFinal(_inputBytes);
-			
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidKeySpecException | InvalidAlgorithmParameterException e) {
-			e.printStackTrace();
-		}
-		
-		return new String(_outputBytes, StandardCharsets.UTF_8);
-	}
+  public String encrypt(String text) {
+    byte[] inputBytes = text.getBytes();
+    byte[] outputBytes = new byte[inputBytes.length];
+    
+    try {
+      cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+      outputBytes = cipher.doFinal(inputBytes);
+      
+    } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
+      e.printStackTrace();
+    }
+    
+    _inputBytes = inputBytes;
+    _outputBytes = outputBytes;
+    return Base64.getEncoder().encodeToString(outputBytes);
+  }
+  
+  public String decrypt(String text) {
+    _inputBytes = Base64.getDecoder().decode(text);
+    
+    try {
+      cipher.init(Cipher.DECRYPT_MODE, keySpec, iv);
+      _outputBytes = cipher.doFinal(_inputBytes);
+      
+    } catch (InvalidKeyException | BadPaddingException | IllegalBlockSizeException | InvalidAlgorithmParameterException e) {
+      e.printStackTrace();
+    }
+    
+    return new String(_outputBytes, StandardCharsets.UTF_8);
+  }
 }
